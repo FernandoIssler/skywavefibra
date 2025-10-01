d7bae9185ee94f8bedbd4ee1fd09cbb64d62d421
Fernando Issler <fernandoissler@hotmail.com>
2025-10-01 18:10:24 -0300

    Alteração do link de Planos da página principal

--- themes/web/home.php
+++ themes/web/home.php
@@ -85,8 +85,7 @@
                             <div class="d-lg-block" id="kt_header_nav_wrapper">
                                 <div class="d-lg-block p-5 p-lg-0" data-kt-drawer="true" data-kt-drawer-name="landing-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="200px" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_landing_menu_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav_wrapper'}">
                                     <!--begin::Menu-->
-                                    <div class="menu menu-column flex-nowrap menu-rounded menu-lg-row menu-title-gray-600 
-            menu-state-title-primary nav nav-flush fs-5 fw-semibold" id="kt_landing_menu">
+                                    <div class="menu menu-column flex-nowrap menu-rounded menu-lg-row menu-title-gray-600 menu-state-title-primary nav nav-flush fs-5 fw-semibold" id="kt_landing_menu">
 
                                         <!--begin::Menu item-->
                                         <div class="menu-item">
@@ -169,7 +168,7 @@
                             </h1>
                             <!--end::Title-->
                             <!--begin::Action-->
-                            <a href="#pricing" class="btn btn-primary btn-lg">
+                            <a href="#pricing" data-kt-scroll-toggle="true" class="btn btn-primary btn-lg">
                                 Conheça nossos Planos
                             </a>
                             <!--end::Action-->
@@ -1109,7 +1108,8 @@
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-            const links = document.querySelectorAll('#kt_landing_menu a[href^="#"]');
+            // Pega todos os links que possuem href começando com #
+            const links = document.querySelectorAll('a[href^="#"]');
 
             links.forEach(link => {
                 link.addEventListener("click", function(e) {
@@ -1119,9 +1119,8 @@
                     const targetElement = document.getElementById(targetId);
 
                     if (targetElement) {
-                        // scroll suave até a seção
                         window.scrollTo({
-                            top: targetElement.offsetTop - 80, // ajuste o -80 se tiver header fixo
+                            top: targetElement.offsetTop - 80, // ajuste caso tenha header fixo
                             behavior: "smooth"
                         });
                     }
@@ -1129,6 +1128,7 @@
             });
         });
     </script>
+
 
 
 </body>
